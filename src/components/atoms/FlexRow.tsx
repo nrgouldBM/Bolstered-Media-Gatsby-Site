2b889a1a4340efd69408e6edee08dd069059f40b
nrgouldBM <nicholas@bolsteredmedia.com>
2022-02-04 09:29:23 -0500

    added netlify.toml

--- src/components/atoms/FlexRow.tsx
+++ src/components/atoms/FlexRow.tsx
@@ -1,20 +1,27 @@
-
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 export const StyledRow = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: ${p => p.justifyContent};
-    align-items: ${p => p.alignItems};
-    flex-wrap: ${p => p.wrap || "no-wrap"};
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: ${(p) => p.justifyContent};
+  align-items: ${(p) => p.alignItems};
+  flex-wrap: ${(p) => p.wrap || "no-wrap"};
+`;
 
-
-export default function FlexRow({ children, alignItems, justifyContent, wrap }) {
-    return (
-        <StyledRow alignItems={alignItems} justifyContent={justifyContent} wrap={wrap}>
-            {children}
-        </StyledRow>
-    )
+export default function FlexRow({
+  children,
+  alignItems,
+  justifyContent,
+  wrap,
+}) {
+  return (
+    <StyledRow
+      alignItems={alignItems}
+      justifyContent={justifyContent}
+      wrap={wrap}
+    >
+      {children}
+    </StyledRow>
+  );
 }
