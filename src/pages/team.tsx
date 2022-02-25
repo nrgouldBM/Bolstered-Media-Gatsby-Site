31e2d5797b8b5c7e2a7e1735bc43bcb13bdb54e0
nrgouldBM <nicholas@bolsteredmedia.com>
2022-02-25 11:06:06 -0500

    team query

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -7,8 +7,10 @@ import NavbarMobile from "../components/organisms/NavbarMobile";
 import TeamMembers from "../components/organisms/TeamMembers";
 import { Main } from "../components/atoms/Main";
 import Footer from "../components/organisms/Footer";
+import { graphql } from "gatsby";
 
-export default function Team() {
+export default function Team({ data }) {
+  console.log({ data });
   const { width } = useWindowDimensions();
   return (
     <React.Fragment>
@@ -21,3 +23,9 @@ export default function Team() {
     </React.Fragment>
   );
 }
+
+export const pageQuery = graphql`
+  query {
+    team
+  }
+`;
