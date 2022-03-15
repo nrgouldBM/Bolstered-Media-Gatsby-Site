55501887dfaec070ad18827b8f1ea8ed73f09294
nrgould <nrgould197@gmail.com>
2022-03-15 13:35:57 -0400

    animated card

---
+++ src/components/atoms/AnimatedCard.tsx
@@ -0,0 +1,41 @@
+import styled, { css } from "styled-components";
+import { COLORS, SHADOW } from "../../theme";
+
+interface styleProps {
+  flexDir?: any;
+  alignItems?: any;
+  justifyContent?: any;
+  width?: any;
+  height?: any;
+  secondary?: boolean;
+  hoverStyles?: boolean;
+}
+
+const hoverStyles = css<styleProps>`
+  transition: all 500ms ease;
+  &:hover {
+    background-color: ${COLORS.primary3};
+  }
+`;
+
+const flexStyles = css<styleProps>`
+  display: flex;
+  flex-direction: ${(p) => p.flexDir};
+  align-items: ${(p) => p.alignItems};
+  justify-content: ${(p) => p.justifyContent};
+`;
+
+export const AnimatedCard = styled.div<styleProps>`
+  /* overflow: hidden; */
+  border-radius: 16px;
+  width: ${(p) => p.width || "20rem"};
+  min-width: 20rem;
+  height: ${(p) => p.height || "30rem"};
+  background-color: ${(p) => (p.secondary ? COLORS.gray : "#fff")};
+  box-shadow: ${SHADOW.normal};
+  ${(p) => p.flexDir && flexStyles}
+  padding: 3rem 2rem;
+  margin: 2rem 1rem;
+  transition: all 200ms ease;
+  ${(p) => p.hoverStyles && hoverStyles}
+`;
--- src/components/molecules/ServicesSection.tsx
+++ src/components/molecules/ServicesSection.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import Card from "../atoms/Card";
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
 import Icon from "./Icon";
 import ButtonLink from "../atoms/ButtonLink";
 import { FaArrowRight } from "react-icons/fa";
 import { Title } from "../atoms/Title";
 import { Body } from "../atoms/Body";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
+import { AnimatedCard } from "../atoms/AnimatedCard";
 
 const ServiceContainer = styled.section`
   padding: 3rem 5rem;
@@ -26,11 +28,32 @@ interface Props {
 
 export default function ServicesSection({ title, content, id, icon }: Props) {
   const { width } = useWindowDimensions();
+  const animationControl = useAnimation();
+  const { inView, entry, ref } = useInView();
+
+  if (inView) {
+    animationControl.start({
+      x: 0,
+      rotateZ: 0,
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+      },
+    });
+  }
+
   return (
-    <ServiceContainer style={{ backgroundColor: icon.bgColor }} id={id}>
-      <Card
+    <ServiceContainer
+      ref={ref}
+      style={{ backgroundColor: icon.bgColor }}
+      id={id}
+    >
+      <AnimatedCard
+        as={motion.div}
+        initial={{ x: 100, opacity: 0, rotateZ: 2 }}
         width={width / 2.3 + "px"}
         height="fit-content"
+        animate={animationControl}
         style={{
           minWidth: "18rem",
           minHeight: "25rem",
@@ -52,7 +75,7 @@ export default function ServicesSection({ title, content, id, icon }: Props) {
           style={{ width: "15rem", marginTop: "2rem" }}
           icon={<FaArrowRight color={"#fff"} size={20} />}
         />
-      </Card>
+      </AnimatedCard>
     </ServiceContainer>
   );
 }
