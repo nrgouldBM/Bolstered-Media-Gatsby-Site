9d32aabefe304d020070e657582179d61f434639
nrgould <nrgould197@gmail.com>
2022-03-28 12:26:30 -0400

    fixed logos

--- src/components/organisms/BrandLogos.tsx
+++ src/components/organisms/BrandLogos.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import UbumpLogo from "../../images/logos/ubump.png";
 import PineapplesLogo from "../../images/logos/21pineapples.png";
-import AshleyNicholeLogo from "../../images/logos/ashleynichole.png";
 import CoastLandLogo from "../../images/logos/coastland.png";
 import FTSLogo from "../../images/logos/fts.png";
 import OldSouthLogo from "../../images/logos/old-south.png";
+import SouthernAttitudeLogo from "../../images/logos/SA.png";
 import { breakpoint, COLORS } from "../../theme";
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
 
 const Container = styled.section`
   margin: auto;
   margin-top: 5rem;
-  background-color: ${COLORS.gray};
+  background-color: #101010;
   box-sizing: border-box;
   padding: 2.5rem 0;
   max-width: 100vw;
@@ -29,16 +29,21 @@ const LogosContainer = styled.div`
 
 const Logo = styled.img`
   max-width: 12rem;
+  transition: all 200ms ease;
 
   @media (max-width: ${breakpoint + "px"}) {
     margin: 0 2rem 3rem 2rem;
   }
+
+  &:hover {
+    transform: scale(1.05);
+  }
 `;
 
 const LogosText = styled.h3`
-  color: ${COLORS.secondaryText};
+  color: ${COLORS.primary3};
   font-size: 1.2rem;
-  font-weight: 500;
+  font-weight: 600;
   margin-bottom: 2rem;
   text-align: center;
   /* padding-top: 2rem; */
@@ -48,9 +53,9 @@ const LOGOS = [
   { alt: "Old South Logo", image: OldSouthLogo },
   { alt: "uBump logo", image: UbumpLogo },
   { alt: "21Pineapples Apparel Logo ", image: PineapplesLogo },
-  { alt: "Ashley Nichole Logo", image: AshleyNicholeLogo },
   { alt: "Failure To Stop Logo", image: FTSLogo },
   { alt: "Coatland Apparel Logo", image: CoastLandLogo },
+  { alt: "Southern Attitude Logo", image: SouthernAttitudeLogo },
 ];
 
 export default function BrandLogos() {
