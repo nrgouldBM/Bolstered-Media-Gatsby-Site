e5c2a6b04db51dc8f044991eab551a29439f1259
nrgould <nrgould197@gmail.com>
2022-11-18 11:09:39 -0500

    added netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,12 +31,21 @@ module.exports = {
         lang: "en",
       },
     },
-    // "gatsby-plugin-netlify-cms",
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        /**
+         * One convention is to place your Netlify CMS customization code in a
+         * `src/cms` directory.
+         */
+        // modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     // {
     //   resolve: "gatsby-source-filesystem",
     //   options: {
     //     name: `markdown-pages`,
-    //     path: `${__dirname}/blog`,
+    //     path: `${__dirname}/case-studies`,
     //   },
     // },
     {
